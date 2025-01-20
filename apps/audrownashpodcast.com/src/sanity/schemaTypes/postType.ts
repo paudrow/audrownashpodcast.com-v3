b21ccc095085d50e5d1bc5f626639456b874443d
Audrow Nash <audrow@hey.com>
2025-01-20 10:27:29 -0600

    Add related posts

--- apps/audrownashpodcast.com/src/sanity/schemaTypes/postType.ts
+++ apps/audrownashpodcast.com/src/sanity/schemaTypes/postType.ts
@@ -50,6 +50,11 @@ export const postType = defineType({
       name: 'body',
       type: 'blockContent',
     }),
+    defineField({
+      name: "relatedPosts",
+      type: "array",
+      of: [{ type: "reference", to: { type: "post" } }],
+    }),
   ],
   preview: {
     select: {
